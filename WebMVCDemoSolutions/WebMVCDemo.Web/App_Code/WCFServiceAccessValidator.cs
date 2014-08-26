ab7724e3d1bc5c32694ae3838fb35e9c540f0e59
KapsPro <Kapil.b.khadgi@gmail.com>
2014-08-26 19:08:06 +0530

    Attempt to make WCF service secure.

---
+++ WebMVCDemoSolutions/WebMVCDemo.Web/App_Code/WCFServiceAccessValidator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Web;
+
+namespace WebMVCDemo.Web.App_Code
+{
+    public class WCFServiceAccessValidator : System.IdentityModel.Selectors.UserNamePasswordValidator
+    {
+        public override void Validate(string userName, string password)
+        {
+            if (userName == null || password == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            if (!(userName == "fayaz" && password == "fayaz1"))
+            {
+                throw new FaultException("Incorrect Username or Password");
+            }
+        }
+    }
+}
