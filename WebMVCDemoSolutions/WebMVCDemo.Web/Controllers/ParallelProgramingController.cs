e5eed3829e34842a1a1f9b97bf91689ee181c9fa
KapsPro <Kapil.b.khadgi@gmail.com>
2014-10-22 14:32:24 +0530

    Added code to show async programming

---
+++ WebMVCDemoSolutions/WebMVCDemo.Web/Controllers/ParallelProgramingController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebMVCDemo.Web.Controllers
+{
+    public class ParallelProgramingController : Controller
+    {
+        // GET: ParallelPrograming
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public async static Task<long?> GetPageLength()
+        {
+            HttpClient client = new HttpClient();
+            var httpMessage = await client.GetAsync("http://apress.com");
+            return httpMessage.Content.Headers.ContentLength;
+        } 
+    }
+}
